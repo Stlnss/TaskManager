07053edc1ecdb06ccc3abe41b06f9c4c1ad70eab
stlnss <stlnesslv@gmail.com>
2023-04-16 18:03:58 +1000

    Add all tasks complete screen

--- app/src/main/java/com/still/taskmanager/MainActivity.kt
+++ app/src/main/java/com/still/taskmanager/MainActivity.kt
@@ -3,13 +3,21 @@ package com.still.taskmanager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.still.taskmanager.ui.theme.TaskManagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +30,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    AllTaskCompleteScreen()
                 }
             }
         }
@@ -30,14 +38,49 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun AllTaskCompleteScreen() {
+    CompleteCard(
+        image = painterResource(id = R.drawable.ic_task_completed),
+        allCompleteText = stringResource(id = R.string.all_tasks_completed),
+        niceWorkText = stringResource(id = R.string.nice_work)
+    )
+}
+
+@Composable
+fun CompleteCard(
+    modifier: Modifier = Modifier,
+    image: Painter,
+    allCompleteText: String,
+    niceWorkText: String
+) {
+    Column(
+        modifier = modifier
+            .fillMaxHeight()
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(painter = image, contentDescription = null)
+        Text(
+            text = allCompleteText,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = modifier.padding(
+                top = 24.dp,
+                bottom = 8.dp
+            )
+        )
+        Text(
+            text = niceWorkText,
+            fontSize = 16.sp
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     TaskManagerTheme {
-        Greeting("Android")
+        AllTaskCompleteScreen()
     }
 }
